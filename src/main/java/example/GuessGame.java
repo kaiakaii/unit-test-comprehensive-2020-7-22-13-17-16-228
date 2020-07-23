678a0e2edf4bc50178e6817df8ed5321bccef7fd
sky <senkk512@163.com>
2020-07-23 20:44:26 +0800

    [GuessGame][Sky] feature:add should_return_2A1B_when_guess_game_given_1253 test

--- src/main/java/example/GuessGame.java
+++ src/main/java/example/GuessGame.java
@@ -1,19 +1,38 @@
 package example;
 
-public class GuessGame implements GenerateRandomNum{
+public class GuessGame implements GenerateRandomNum {
 
-    public String guess(String guess, String generateNum) {
-        if(guess.equals(generateNum)){
-            return "4A0B";
-        }
-        return "2A2B";
+    private final String randomNum;
+
+    public GuessGame(String randomNum) {
+        this.randomNum = randomNum;
     }
 
-    public String generateRandomNum(){
+    public String guess(String guess, String generateNum) {
+        if (guess.equals(generateNum)) {
+            return "4A0B";
+        }
+        int countA = 0;
+        int countB = 0;
+        String[] guessArray = guess.split("");
+        String[] randomArray = generateNum.split("");
+        for (int i = 0; i < guessArray.length; i++) {
+            if (guessArray[i].equals(randomArray[i])) {
+                countA++;
+            }
+            if (generateNum.contains(guessArray[i]) && !guessArray[i].equals(randomArray[i])) {
+                countB++;
+            }
+        }
+
+        return countA + "A" + countB + "B";
+    }
+
+    public String generateRandomNum() {
         return "";
     }
 
-    public boolean isValid(String guess){
+    public boolean isValid(String guess) {
 
         return false;
     }
--- src/test/java/example/GuessGameTest.java
+++ src/test/java/example/GuessGameTest.java
@@ -10,10 +10,10 @@ public class GuessGameTest {
     @Test
     public void should_return_4A0B_when_guess_game_given_1234(){
         //given
-        GuessGame guessGame = new GuessGame();
         GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
         given(generateRandomNum.generateRandomNum()).willReturn("1234");
         String randomNum = generateRandomNum.generateRandomNum();
+        GuessGame guessGame = new GuessGame(randomNum);
 
         //when
         String guess = guessGame.guess("1234",randomNum);
@@ -24,10 +24,10 @@ public class GuessGameTest {
     @Test
     public void should_return_2A2B_when_guess_game_given_1243(){
         //given
-        GuessGame guessGame = new GuessGame();
         GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
         given(generateRandomNum.generateRandomNum()).willReturn("1234");
         String randomNum = generateRandomNum.generateRandomNum();
+        GuessGame guessGame = new GuessGame(randomNum);
 
         //when
         String guess = guessGame.guess("1243",randomNum);
@@ -35,21 +35,21 @@ public class GuessGameTest {
         //then
         assertEquals("2A2B",guess);
     }
-//    @Test
-//    public void should_return_2A1B_when_guess_game_given_1253(){
-//        //given
-//        GuessGame guessGame = new GuessGame();
-//        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
-//        given(generateRandomNum.generateRandomNum()).willReturn("1234");
-//        String randomNum = generateRandomNum.generateRandomNum();
-//
-//        //when
-//        String guess = guessGame.guess("1234",randomNum);
-//
-//        //then
-//        assertEquals("4A0B",guess);
-//
-//    }
+    @Test
+    public void should_return_2A1B_when_guess_game_given_1253(){
+        //given
+        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
+        given(generateRandomNum.generateRandomNum()).willReturn("1234");
+        String randomNum = generateRandomNum.generateRandomNum();
+        GuessGame guessGame = new GuessGame(randomNum);
+
+        //when
+        String guess = guessGame.guess("1253",randomNum);
+
+        //then
+        assertEquals("2A1B",guess);
+
+    }
 //    @Test
 //    public void should_return_0A4B_when_guess_game_given_4321(){
 //        //given
