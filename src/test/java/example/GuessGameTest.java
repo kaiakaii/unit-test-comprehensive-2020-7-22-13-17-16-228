05156fcf3d2d18e663bbd8beccd0432cd5c8eff4
sky <senkk512@163.com>
2020-07-23 21:03:28 +0800

    [GuessGame][Sky] feature:add should_return_0A2B_when_guess_game_given_2167 test

--- src/test/java/example/GuessGameTest.java
+++ src/test/java/example/GuessGameTest.java
@@ -65,20 +65,20 @@ public class GuessGameTest {
         assertEquals("0A4B",guess);
 
     }
-//    @Test
-//    public void should_return_0A2B_when_guess_game_given_2167(){
-//        //given
-//        GuessGame guessGame = new GuessGame();
-//        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
-//        given(generateRandomNum.generateRandomNum()).willReturn("1234");
-//        String randomNum = generateRandomNum.generateRandomNum();
-//
-//        //when
-//        String guess = guessGame.guess("1234",randomNum);
-//
-//        //then
-//        assertEquals("4A0B",guess);
-//    }
+    @Test
+    public void should_return_0A2B_when_guess_game_given_2167(){
+        //given
+        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
+        given(generateRandomNum.generateRandomNum()).willReturn("1234");
+        String randomNum = generateRandomNum.generateRandomNum();
+        GuessGame guessGame = new GuessGame(randomNum);
+
+        //when
+        String guess = guessGame.guess("2167",randomNum);
+
+        //then
+        assertEquals("0A2B",guess);
+    }
 //    @Test
 //    public void should_return_0A0B_when_guess_game_given_5678(){
 //        //given
