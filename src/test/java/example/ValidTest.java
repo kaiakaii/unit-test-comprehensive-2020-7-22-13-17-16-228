2285e5c6ac7b07eb7a14a6ff9d04ca09cf72d4d9
SUNSK <senkk512@163.com>
2020-07-24 11:08:24 +0800

    [GuessName][Sky] refactor :add should_false_when_isValid_given_abc test

--- src/test/java/example/ValidTest.java
+++ src/test/java/example/ValidTest.java
@@ -20,19 +20,24 @@ public class ValidTest {
     @Test
     void should_false_when_isValid_given_abc() {
         //given
+        Valid valid = new ValidImpl();
 
         //when
+        boolean result = valid.isValidNum("abc");
 
         //then
+        assertEquals(false,result);
 
     }
     @Test
     void should_false_when_isValid_given_1125() {
         //given
+        Valid valid = new ValidImpl();
 
         //when
+        boolean result = valid.isValidNum("1125");
 
         //then
-
+        assertEquals(false,result);
     }
 }
