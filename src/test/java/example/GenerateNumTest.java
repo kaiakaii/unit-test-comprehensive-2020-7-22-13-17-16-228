ba5bfc4fa8326eaedd44aaa232185279837ba1db
SUNSK <senkk512@163.com>
2020-07-24 12:59:28 +0800

    [GuessName][Sky] refactor :add should_false_when_generate_num_is_valid_given_1124 test

--- src/test/java/example/GenerateNumTest.java
+++ src/test/java/example/GenerateNumTest.java
@@ -1,19 +1,22 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 public class GenerateNumTest {
     @Test
-    void should_return_false_when_generate_num_given_random() {
+    void should_return_false_when_generate_num_given_1124() {
         //given
-       // Valid valid = new ValidImpl();
-       // GenerateRandomNum generateRandomNum = mock
+        Valid valid = new ValidImpl();
+        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
+        given(generateRandomNum.generateRandomNum()).willReturn("1124");
         //when
-       // boolean actual = valid.isValidNum()
+        boolean actual = valid.isValidNum(generateRandomNum.generateRandomNum());
 
         //then
-        //assertEquals(false,actual);
+        assertEquals(false,actual);
     }
 }
